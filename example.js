1b9b2685688d5b34d2cc43b020d094d256a57ea5
nswbmw <gxqzk@126.com>
2019-05-09 10:58:10 +0800

    bump v1.8.1

--- example.js
+++ example.js
@@ -7,7 +7,7 @@ app.use(mongo())
 app.use(async (ctx, next) => {
   await ctx.mongo.db('test').collection('users').insertOne({ name: 'example' })
   ctx.body = await ctx.db.collection('users').find().toArray()
-  await ctx.db.collection('users').removeMany({ name: 'example' })
+  // await ctx.db.collection('users').removeMany({ name: 'example' })
 })
 
 app.listen(3000, () => {
